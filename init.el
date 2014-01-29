1f3faaaf0f791fef093d9332103a77549d0b25a4
Achal Shah <anshah@amazon.com>
2014-01-28 16:54:48 -0800

    auto-indent

--- init.el
+++ init.el
@@ -23,7 +23,9 @@
   ;; If you edit it by hand, you could mess it up, so be careful.
   ;; Your init file should contain only one such instance.
   ;; If there is more than one, they won't work right.
- '(inhibit-startup-screen t))
+ '(inhibit-startup-screen t)
+ '(indent-tabs-mode nil)
+)
 (custom-set-faces
   ;; custom-set-faces was added by Custom.
   ;; If you edit it by hand, you could mess it up, so be careful.
@@ -38,3 +40,8 @@
 (require 'color-theme)
 (color-theme-initialize)
 (color-theme-dark-laptop)
+
+(setq-default tab-width 4)
+(defun my-c++-indent-setup ()
+  (setq c-basic-offset 4)
+  (setq indent-tabs-mode nil))
